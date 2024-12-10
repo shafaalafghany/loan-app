2a5d6342148ecb3af3f814d941e671259344f3b2
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-12-10 20:22:47 +0700

    refactor (user): adjust logic for register new user

--- service/user_service.go
+++ service/user_service.go
@@ -10,7 +10,7 @@ import (
 )
 
 type UserServiceInterface interface {
-	Register(*fiber.Ctx, *model.User) error
+	Register(*fiber.Ctx, *model.UserRequest) error
 }
 
 type UserService struct {
@@ -25,9 +25,33 @@ func NewUserService(repo repository.UserRepositoryInterface, log *zap.Logger) Us
 	}
 }
 
-func (u *UserService) Register(c *fiber.Ctx, data *model.User) error {
-	u.log.Info("incoming request to register new user", zap.Any("data", data))
-	data.ID = uuid.New()
+func (u *UserService) Register(c *fiber.Ctx, body *model.UserRequest) error {
+	u.log.Info("incoming request to register new user", zap.Any("data", body))
+
+	exists, err := u.repo.GetByEmail(body.Email)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	if exists.Email == body.Email {
+		u.log.Error("email already exists")
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"message": "email already exists",
+		})
+	}
+
+	data := &model.User{
+		ID:           uuid.New(),
+		Email:        body.Email,
+		NIK:          body.NIK,
+		FullName:     body.FullName,
+		LegalName:    body.LegalName,
+		TempatLahir:  body.TempatLahir,
+		TanggalLahir: body.TanggalLahir,
+		Gaji:         body.Gaji,
+	}
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(data.Password), bcrypt.DefaultCost)
 	if err != nil {
@@ -40,11 +64,12 @@ func (u *UserService) Register(c *fiber.Ctx, data *model.User) error {
 	data.Password = string(hash)
 
 	if err := u.repo.Create(data); err != nil {
-
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": "failed to create new user data",
 		})
 	}
 
-	return nil
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "create new user successfully",
+	})
 }
