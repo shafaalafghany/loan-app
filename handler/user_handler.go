77db18eb1f9c1e5259d94af966555efbf2ca6ac1
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-12-10 14:26:20 +0700

    feat (user): add function register for user handler

---
+++ handler/user_handler.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/shafaalafghany/loan-app/model"
+	"github.com/shafaalafghany/loan-app/service"
+	"go.uber.org/zap"
+)
+
+type UserHandler struct {
+	s   service.UserServiceInterface
+	log *zap.Logger
+}
+
+func NewUserHandler(s service.UserServiceInterface, log *zap.Logger) *UserHandler {
+	return &UserHandler{
+		s:   s,
+		log: log,
+	}
+}
+
+func (h *UserHandler) Register(c *fiber.Ctx) error {
+	h.log.Info("incoming request in handler for register new user")
+	var user model.User
+	if err := c.BodyParser(&user); err != nil {
+		fmt.Println("error", err)
+		return c.Status(fiber.StatusBadRequest).
+			SendString("failed to parsing payload")
+	}
+
+	if err := h.s.Register(c, &user); err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "create new user successfully",
+	})
+}
